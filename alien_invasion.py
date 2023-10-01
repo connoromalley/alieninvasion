76591f8740e4c78885a5d9b1741047a8b1234cb3
connoromalley <william.omalley@umconnect.umt.edu>
2023-09-30 22:11:13 -0600

    aliens move and bullets work and remove aliens on collisions

--- alien_invasion.py
+++ alien_invasion.py
@@ -44,12 +44,26 @@ class AlienInvasion:
             current_x = alien_width
             current_y += alien_height
             
-    def _create_alien(self, x_position):
-        """Create an alien and plac it in the row."""
+    def _create_alien(self, x_position, y_position):
+        """Create an alien and place it in the fleet."""
         new_alien = Alien(self)
         new_alien.x = x_position 
         new_alien.rect.x = x_position
+        new_alien.rect.y = y_position
         self.aliens.add(new_alien)
+
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Drop the entire fleetand change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -57,6 +71,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen() 
             self.clock.tick(60)
 
@@ -105,6 +120,16 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
         
+        # check for any bullets that have hit aliens.
+        # if so, get rid ofthe bullet and the alien.
+        collisions = pygame.sprite.groupcollide(
+            self.bullets, self.aliens, True, True)
+        
+    def _update_aliens(self):
+        """Check ifthe fleet is at an edge, then update positions"""
+        self._check_fleet_edges()
+        self.aliens.update()
+    
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
