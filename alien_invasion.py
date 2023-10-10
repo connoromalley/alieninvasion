d77fa6cd6c532028acdd87a7d3a218a1a877565d
connoromalley <william.omalley@umconnect.umt.edu>
2023-10-09 22:56:44 -0600

    it works and its done!

--- alien_invasion.py
+++ alien_invasion.py
@@ -84,8 +84,9 @@ class AlienInvasion:
         """Respond to the ship being hit by an alien."""
         if self.stats.ships_left > 0:
             
-            # decrement ships_left
+            # decrement ships_left and update scoreboard.
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             # get rid of any remaining bullets and aliens.
             self.bullets.empty()
@@ -142,10 +143,11 @@ class AlienInvasion:
             # Reset the game settings
             self.settings.initialize_dynamic_settings() # does this need to be removed???
 
-
             # Reset the game statistics.
             self.stats.reset_stats()
             self.sb.prep_score()
+            self.sb.prep_level()
+            self.sb.prep_ships()
             self.game_active = True 
 
             #Get rid of any remaining bullets and aliens.
@@ -202,14 +204,20 @@ class AlienInvasion:
             self.bullets, self.aliens, True, True)
 
         if collisions:
-            self.stats.score += self.settings.alien_points
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
+            self.sb.check_high_score()
 
         if not self.aliens:
             # Destroy existing bullets and create new feet.
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            # Increase level
+            self.stats.level += 1
+            self.sb.prep_level()
 
     def _update_aliens(self):
         """Check ifthe fleet is at an edge, then update positions"""
--- settings.py
+++ settings.py
@@ -19,7 +19,7 @@ class Settings:
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60,60,60)
-        self.bullets_allowed = 3
+        self.bullets_allowed = 30
 
         # Alien settings
         self.alien_speed = 1.0
@@ -28,14 +28,17 @@ class Settings:
         # How quickly the game speeds up.
         self.speedup_scale = 1.1
 
+        # How quickly the alien point values increas
+        self.score_scale = 1.5
+
         self.initialize_dynamic_settings()
 
         
     def initialize_dynamic_settings(self):
         """Initialize settings that change throughout the game."""
-        self.ship_speed = 1.5
-        self.bullet_speed = 2.5
-        self.alien_speed = 1.0
+        self.ship_speed = 5
+        self.bullet_speed = 3
+        self.alien_speed = 3
 
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
@@ -44,10 +47,14 @@ class Settings:
         self.alien_points = 50
         
     def increase_speed(self):
-        """Increase speed settings."""
+        """Increase speed settings and alien point values."""
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
+        print(self.alien_points)
+
 
 
 
